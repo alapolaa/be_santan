56a8d57134d1c04c4517507b2505388118eb9823
alapolaa <zain.alapolaa@gmail.com>
2025-02-13 04:23:07 -0800

    get api cuaca

---
+++ api/get_musim.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+
+include '../koneksi.php';
+
+$sql = "SELECT * FROM musim";
+$result = mysqli_query($conn, $sql);
+
+$data = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+
+mysqli_close($conn);
